0cc67bb447e4bd2891e1d26c19dfea6c5e67bbc5
ryunosuke0922 <ryumaeda0922@gmail.com>
2023-03-18 08:24:20 +0900

    fix

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -2,18 +2,20 @@ import { FiTwitter } from 'react-icons/fi';
 
 const Footer = (): JSX.Element => {
   return (
-    <footer className="bg-black py-8 text-gray-100">
+    <footer className="bg-black py-6 text-sm text-gray-100">
       <div className="mx-auto flex max-w-7xl flex-col items-center justify-between px-4 sm:px-6 md:flex-row lg:px-8">
-        <a
-          href="/privacy-policy"
-          className="mb-4 text-sm hover:text-gray-300 md:mb-0"
-        >
+        <a href="/privacy-policy" className="mb-4 hover:text-gray-300 md:mb-0">
           プライバシーポリシー
         </a>
-        <div className="flex items-center">
-          <FiTwitter className="h-6 w-6 cursor-pointer text-blue-500 hover:text-blue-400" />
-          <span className="ml-2 text-sm">作者: ryunosuke092_</span>
-        </div>
+        <a
+          href="https://twitter.com/ryunosuke092_"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center"
+        >
+          <FiTwitter className="h-4 w-4 cursor-pointer text-blue-500 hover:text-blue-400" />
+          <span className="ml-2">作者</span>
+        </a>
       </div>
     </footer>
   );
--- src/components/organisms/templateLog/index.tsx
+++ src/components/organisms/templateLog/index.tsx
@@ -10,12 +10,12 @@ type TemplateComponentProps = {
 
 const TemplateLog: React.FC<TemplateComponentProps> = ({ formData }) => {
   return (
-    <div className="rounded-lg bg-black p-6 shadow-lg">
-      <h2 className="mb-4 text-3xl font-bold text-white">
+    <div className="rounded-lg bg-black p-4 shadow-lg">
+      <h2 className="mb-2 text-2xl font-bold text-white">
         作成中のテンプレート
       </h2>
-      <div className="rounded-md bg-white p-4 text-gray-800">
-        <code className="whitespace-pre-wrap break-all font-mono text-sm">
+      <div className="rounded-md bg-white p-2 text-gray-800">
+        <code className="whitespace-pre-wrap break-all font-mono text-xs">
           {convertFormDataToText(formData)}
         </code>
       </div>
